a4700034616ce7f86c8e89705b86a52eaffb0557
Martin Matovu <martinthewacky@gmail.com>
2022-04-25 11:03:34 +0300

    Edit index.js

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,22 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
+import Head from "next/head";
+import Layout from "../components/Layout";
 
 export default function Home() {
   return (
-    <Layout home>
+    <Layout>
       <Head>
-        <title>{siteTitle}</title>
+        <title>Wacky Dev: The Home of The Wackiest Dev Alive</title>
+        <meta
+          name="description"
+          content="The home of Wacky Dev welcomes you with unbelievable gratitude."
+        />
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
+      <div className="w-full h-full">
+        <section className="w-full px-3">
+          <h1 className="">WELCOME</h1>
+          <h2 className="">WEB TRAVERSER!</h2>
+        </section>
+      </div>
     </Layout>
-  )
+  );
 }
