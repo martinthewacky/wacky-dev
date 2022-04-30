58aee93ae49d299abbe472122bb271a651feddcc
Martin Matovu <martinthewacky@gmail.com>
2022-04-30 10:30:10 +0300

    Edit Modal.js for nav list

--- components/Modal/NavElements.js
+++ components/Modal/NavElements.js
@@ -3,9 +3,11 @@ import NavData from "./NavData";
 
 const NavList = NavData.map((element, index) => {
   return (
-    <li className="w-full" key={index}>
-      <Link href={element.url} className="w-full p-3">
-        <a>{element.textContent}</a>
+    <li className="w-full flex justify-center items-center" key={index}>
+      <Link href={element.url} className="w-full h-full">
+        <a className="w-full h-full text-center">
+          <span className="w-full p-3 text-center">{element.textContent}</span>
+        </a>
       </Link>
     </li>
   );
